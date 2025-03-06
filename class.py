c983df58623fac730837cd085cc0cae132294cb6
Ayeshafatima123 <160357945+Ayeshafatima123@users.noreply.github.com>
2025-03-07 01:13:45 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,93 @@
+#python Operator " operator are used to perform operation on variables and values
+# arithmetic  operator
+# assignment  operator
+# comparison operator
+# logical operator
+# membership  operator
+# identify operator 
+#  bitwise  operator
+
+# arithmetic operator
+print (10 + 20)
+print (10-20)
+print (10*20)
+print (10/20)
+print (10%20)
+print (10**20)
+print  (10//20)
+#  assignment opertor
+a=10
+print(a)
+a+=30
+print(a)
+a-=20
+print(a)
+a*=20
+print(a)
+a/=30
+print(a)
+a%=30
+print(a)
+a**=30
+print(a)
+a//=30
+print(a) 
+ 
+#  comparing operator
+print (10== 30)
+print (10!=30)
+print (10>30)
+print(10 <30)
+print(10>=30)
+print(10<=30)
+
+# logical operotor
+a=10
+
+print(a>5 and a<11)
+ 
+ 
+ 
+#  membership operator
+a =["apple", "banana"]
+print ("banana"  in a)
+
+x =["apple","banana"]
+print ("orange"in x)
+
+# identify operator 
+a= ["apple", "banana"]
+b= ["apple" "banana"]
+c= a
+print (a is c)
+print(a is b)
+
+# bitwise operator
+a =10
+b =20
+print (a&b)
+
+a= 30
+b =40
+print(a^b)
+
+a= 40
+b= 50
+print(~a)
+
+a=20
+b= 10 
+print(a<<b)
+
+a=10
+b= 40
+print(a>>b)
+
+a=30
+b= 20
+print(a|b)
+
+
+
+
+
